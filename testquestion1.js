a0f92c8782a9a69914fd0a5ea9b297c5b6f8c295
Techical7 <chuayunjie.22@gmail.com>
2025-08-31 09:00:20 +0000

    yunjie final answer

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,28 @@
+let balls = [];
+function setup() {
+  createCanvas(800, 600);
+}
 
+function draw() {
+  background(220);
+
+  for (let i = 0; i < balls.length; i++) {
+    let b = balls[i];
+    b.y = b.y + b.speed;
+    b.speed = b.speed + 0.5;
+
+    fill(b.color);
+    circle(b.x, b.y, 40);
+  }
+}
+
+function mousePressed() {
+
+  let newBall = {
+    x: mouseX,
+    y: mouseY,
+    speed: 0,
+    color: color(random(255), random(255), random(255))
+  };
+  balls.push(newBall);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,48 @@
+let balls = [];
+let floorY = 580;
 
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(220);
+
+  for (let i = 0; i < balls.length; i++) {
+    let b = balls[i];
+
+    // gravity
+    b.speed += 0.5;
+    b.y += b.speed;
+
+    if (b.y > floorY) {
+      b.y = floorY;
+      b.speed *= -0.6;
+    }
+
+    for (let j = 0; j < i; j++) {
+      let o = balls[j];
+      let d = dist(b.x, b.y, o.x, o.y);
+      if (d < 40) { // radius*2 = 40
+        b.y = o.y - 40;
+        b.speed *= -0.6;
+      }
+    }
+
+    fill(b.color);
+    circle(b.x, b.y, 40);
+  }
+
+  fill(100);
+  rect(0, floorY + 20, width, height - floorY);
+}
+
+function mousePressed() {
+  let newBall = {
+    x: mouseX,
+    y: mouseY,
+    speed: 0,
+    color: color(random(255), random(255), random(255))
+  };
+  balls.push(newBall);
+}
